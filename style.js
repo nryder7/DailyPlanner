cfac91496c764bf33bcf695fdedb2d66ca049d16
Nicholas Ryder <nryder7@gmail.com>
2020-08-11 17:07:31 -0400

    set hour rows, time column, input form, save button

---
+++ style.js
@@ -0,0 +1,9 @@
+//var currentDay = $("#currentDay");
+//currentDay = $("#currentDay").text(moment().format("MMMM Do YYYY"));
+
+//$("#currentDay").text(moment().format("MMMM Do YYYY"))
+//$("#time").text(moment().format('LT'))
+//$("#hour").text(moment().format('HH'))
+
+
+
